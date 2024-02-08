a0f3a71f89b5c2df29822f995697c41236971957
pkrishnavamsi0709 <pkrishnavamsi2001@gmail.com>
2024-02-08 15:18:39 +0530

    menuAlignment changed
    Changed some alignment in the menu

--- components/MenuItem.jsx
+++ components/MenuItem.jsx
@@ -1,16 +1,16 @@
 export function MenuItem(props) {
   return (
     <div className="MenuItem">
-      <div className="ItemImgBox">
+      <div className="ItemImgBox" id="MenuItemImg">
         <img src={props.img} alt="MenuItems" className="ItemImg"></img>
       </div>
 
-      <div>
-        <h3 className="MenuItemHeader">
-          {props.name} .....................
-          <span> </span>
-          <span className="Price"> {props.price}</span>
-        </h3>
+      <div className="MenuItemHeader" id="MenuItemHead">
+        <h3>{props.name} .....</h3>
+        <p className="Price"> {props.price}</p>
+      </div>
+
+      <div className="MenuItemDesc" id="MenuItemDesc">
         <p className="MenuItemDesc">{props.ingredients}</p>
       </div>
     </div>
