430867379b8233f617f4cb425cc9aac850e4d47a
190030207-Ajaykumar <boraajaykumar007@gmail.com>
2024-02-09 12:25:02 +0530

    Update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -6,7 +6,6 @@ import { Hero } from "@/components/Hero";
 import { Menu } from "@/components/Menu";
 import NavBar from "@/components/NavBar";
 import { Scroll } from "@/components/Scroll";
-import SpecailsSample from "@/components/SpecailsSample";
 import Specials from "@/components/Specials";
 import Testimonial from "@/components/Testmonial";
 import NavBarItems from "@/data/NavBar";
@@ -38,6 +37,8 @@ export default async function Page() {
       <Menu />
       <Testimonial />
       <Scroll />
+      <Booking />
+      <ContactUs />
     </div>
   );
 }
